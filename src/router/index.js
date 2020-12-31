29c1d211c1e5885fa4fb2b617ef2a1de4980d71d
Tobias Hölzer <tobiashoelzer@hotmail.com>
2020-12-31 18:19:48 +0100

    Add AppHeader & First Translations

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,26 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Gallery'
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/profile',
+    name: 'Profile',
+    component: () => import('../views/Profile.vue'),
+    meta: {
+      title: 'Profile'
+    }
+  },
+  {
+    path: '/imprint',
+    name: 'Imprint',
+    component: () => import('../views/Imprint.vue'),
+    meta: {
+      title: 'Imprint'
+    }
   }
 ]
 
@@ -24,6 +35,11 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `Tobias Hölzer - ${to.meta.title || 'Portfolio'}`
+  next()
 })
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import i18n from '../i18n'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    mobile: window.innerWidth <= 800,
+    lang: 'en',
+    languages: [
+      { isoCode: 'de', name: 'Deutsch', selected: false },
+      { isoCode: 'en', name: 'English', selected: true }
+    ]
+  },
+  mutations: {
+    viewChange(state) {
+      state.mobile = window.innerWidth <= 800
+    }
+  },
+  actions: {
+    setLang({ state }, lang) {
+      const isoCodes = state.languages.map(l => l.isoCode)
+      lang = isoCodes.includes(lang) ? lang : 'en'
+      state.lang = lang
+      i18n.locale = lang
+      state.languages.forEach(language => {
+        language.selected = language.isoCode === lang
+      })
+    }
+  },
   modules: {}
 })
