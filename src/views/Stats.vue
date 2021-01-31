9139f3ae73d4d9b3136f25e21e6682842120d7cd
Tobias Hölzer <tobiashoelzer@hotmail.com>
2021-01-31 20:46:16 +0100

    Improve Lingu (Philipp)

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -2,9 +2,6 @@
   <div class="stats">
     <h1>{{ $t('Stats.title') }}</h1>
     <p>{{ $t('Stats.describtion') }}</p>
-    <cv-link href="https://en.wikipedia.org/wiki/Dunning%E2%80%93Kruger_effect" target="_blank">{{
-      $t('Stats.dunning-kruger')
-    }}</cv-link>
     <div class="skill-group-wrapper" v-for="skillGroup in stats" :key="skillGroup.code">
       <SkillGroup :code="skillGroup.code" :skills="skillGroup.skills" />
     </div>
@@ -12,13 +9,12 @@
 </template>
 
 <script>
-import { CvLink } from '@carbon/vue/src/components/cv-link'
 import SkillGroup from '../components/SkillGroup'
 import stats from '../assets/data/stats.json'
 
 export default {
   name: 'stats',
-  components: { CvLink, SkillGroup },
+  components: { SkillGroup },
   data: () => {
     return {
       stats
